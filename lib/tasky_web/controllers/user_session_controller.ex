83897886cc0d3b645392ea2504a40913e9a927b2
Thierry Schmidt <thierry@whatwedo.ch>
2026-03-24 05:55:51 +0100

    Update user_session_controller.ex

--- lib/tasky_web/controllers/user_session_controller.ex
+++ lib/tasky_web/controllers/user_session_controller.ex
@@ -5,33 +5,31 @@ defmodule TaskyWeb.UserSessionController do
   alias TaskyWeb.UserAuth
 
   def create(conn, %{"_action" => "confirmed"} = params) do
-    create(conn, params, "User confirmed successfully.")
+    create(conn, params, nil)
   end
 
   def create(conn, params) do
-    create(conn, params, "Welcome back!")
+    create(conn, params, nil)
   end
 
   # magic link login
-  defp create(conn, %{"user" => %{"token" => token} = user_params}, info) do
+  defp create(conn, %{"user" => %{"token" => token} = user_params}, _info) do
     case Accounts.login_user_by_magic_link(token) do
       {:ok, {user, tokens_to_disconnect}} ->
         UserAuth.disconnect_sessions(tokens_to_disconnect)
 
         conn
-        |> put_flash(:info, info)
         |> UserAuth.log_in_user(user, user_params)
 
       _ ->
         conn
-        |> put_flash(:error, "The link is invalid or it has expired.")
+        |> put_flash(:error, "Der Link ist ungültig oder abgelaufen.")
         |> redirect(to: ~p"/users/log-in")
     end
   end
 
   def delete(conn, _params) do
     conn
-    |> put_flash(:info, "Logged out successfully.")
     |> UserAuth.log_out_user()
   end
 end
