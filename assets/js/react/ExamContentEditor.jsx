86a115fbffa8020a1ff592fb28684e9c58c3d47a
Thierry Schmidt <thierry@MacBook-Pro-von-Thierry.local>
2026-04-24 19:27:12 +0200

    enhance

--- assets/js/react/ExamContentEditor.jsx
+++ assets/js/react/ExamContentEditor.jsx
@@ -121,7 +121,7 @@ const PageBreak = Node.create({
       setPageBreak:
         () =>
         ({ commands }) => {
-          const pageId = Math.floor(Math.random() * 9000) + 1000;
+          const pageId = Math.floor(Math.random() * 90000000) + 10000000;
           return commands.insertContent({
             type: this.name,
             attrs: { pageId },
